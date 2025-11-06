2ba0d76ef7fa3d7b271783ab595c22c5bafd93c5
Thiago Siqueira <thiago.d.santos@bradescoseguros.com.br>
2025-11-06 16:47:34 -0300

    Criado sessão de login simples

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { NavLink, Route, Routes, Navigate } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import MoviesPage from './pages/MoviesPage';
 import TmdbPage from './pages/TmdbPage';
+import LoginPage from './pages/LoginPage';
 import ProtectedRoute from './routes/ProtectedRoute';
 import { AuthProvider } from './contexts/AuthContext'
 import { MoviesProvider } from './contexts/MoviesContext';
+import  NavBar  from './components/NavBar';
 import './index.css'
 
 export default function App() {
   return (
     <AuthProvider>
       <div className='container'>
-        <nav className='header'>
-          <h1>Catálogo de Filmes</h1>
-          <div className='header-actions'>
-            <NavLink className="link" to="/movies">🎬 Filmes</NavLink>
-            <NavLink className="link" to="/tmdb">🎞️ TMDB</NavLink>
-            <a 
-                className='link' 
-                href="https://github.com/thiagoss86/projeto-pos-react"
-                target='_blank'
-                rel='noreferrer'
-            >🐙 GitHub</a>
-            <NavLink className="link" to="/admin">🔐 admin</NavLink>
-          </div>
-        </nav>
-
+        <NavBar />
         <Routes>
           <Route path="/" element={<Navigate to="/movies" replace />} />
           <Route path="/movies" element={<MoviesProvider><MoviesPage /></MoviesProvider>} />
           <Route path="/tmdb" element={<MoviesProvider><TmdbPage /></MoviesProvider>} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/admin"
             element={
               <ProtectedRoute>
